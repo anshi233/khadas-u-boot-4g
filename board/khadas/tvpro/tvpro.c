57a704f45e0be4b1ac4d70ba2a775a8367eaf794
retro98boy <retro98boy@qq.com>
2025-08-27 20:09:00 +0800

    CoreLab A311D2-TVPro: update fdt file name
    Change-Id: I0fe16d28d0c1c956c1cf801c8ee221927d538fcd

--- board/khadas/tvpro/tvpro.c
+++ board/khadas/tvpro/tvpro.c
@@ -154,31 +154,30 @@ static void select_fdtfile(void)
 	case 0x100000000:
 		if (cpu_id.chip_rev == 0x0a || cpu_id.chip_rev == 0x0b) {
 			env_set("chip_recv", "b");
-			env_set("fdtfile", "amlogic/t7-" CONFIG_DEFAULT_DEVICE_TREE "-4g.dtb");
+			env_set("fdtfile", "amlogic/t7-a311d2-" CONFIG_DEFAULT_DEVICE_TREE "-4g.dtb");
 		} else if (cpu_id.chip_rev == 0x0c) {
 			env_set("chip_recv", "c");
-			env_set("fdtfile", "amlogic/t7c-" CONFIG_DEFAULT_DEVICE_TREE "-4g.dtb");
+			env_set("fdtfile", "amlogic/t7c-a311d2-" CONFIG_DEFAULT_DEVICE_TREE "-4g.dtb");
 		}
 		break;
 	
 	case 0x200000000:
 		if (cpu_id.chip_rev == 0x0a || cpu_id.chip_rev == 0x0b) {
 			env_set("chip_recv", "b");
-			env_set("fdtfile", "amlogic/t7-" CONFIG_DEFAULT_DEVICE_TREE "-8g.dtb");
+			env_set("fdtfile", "amlogic/t7-a311d2-" CONFIG_DEFAULT_DEVICE_TREE "-8g.dtb");
 		} else if (cpu_id.chip_rev == 0x0c) {
 			env_set("chip_recv", "c");
-			env_set("fdtfile", "amlogic/t7c-" CONFIG_DEFAULT_DEVICE_TREE "-8g.dtb");
+			env_set("fdtfile", "amlogic/t7c-a311d2-" CONFIG_DEFAULT_DEVICE_TREE "-8g.dtb");
 		}
 		break;
 	
 	default:
 		if (cpu_id.chip_rev == 0x0a || cpu_id.chip_rev == 0x0b) {
 			env_set("chip_recv", "b");
-			env_set("fdtfile", "amlogic/t7-" CONFIG_DEFAULT_DEVICE_TREE "-unsupported.dtb");
 		} else if (cpu_id.chip_rev == 0x0c) {
 			env_set("chip_recv", "c");
-			env_set("fdtfile", "amlogic/t7c-" CONFIG_DEFAULT_DEVICE_TREE "-unsupported.dtb");
 		}
+		env_set("fdtfile", "unsupported.dtb");
 		break;
 	}
 }
