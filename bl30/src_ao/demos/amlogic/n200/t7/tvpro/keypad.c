7b9003cab9b5f932d124a74b08ce1fc53d659e9d
retro98boy <retro98boy@qq.com>
2025-09-18 20:58:30 +0800

    CoreLab A311D2-TVPro: bl30: small clean
    Change-Id: I2f513a91b74b887e97de2c45a204a4e1276b673e

--- bl30/src_ao/demos/amlogic/n200/t7/tvpro/keypad.c
+++ bl30/src_ao/demos/amlogic/n200/t7/tvpro/keypad.c
@@ -8,8 +8,6 @@
 
 /*KEY ID*/
 #define GPIO_KEY_ID_POWER	GPIOD_4
-/*WOL GPIO*/
-#define WOL_GPIO	GPIOH_6
 
 #define ADC_KEY_ID_MENU		520
 #define ADC_KEY_ID_VOL_DEC	521
@@ -24,10 +22,6 @@ static void vGpioKeyCallBack(struct xReportEvent event)
 	switch (event.ulCode) {
 	case GPIO_KEY_ID_POWER:
 		buf[0] = POWER_KEY_WAKEUP;
-		STR_Wakeup_src_Queue_Send(buf);
-		break;
-	case WOL_GPIO:
-		buf[0] = WOL_WAKEUP;
 		STR_Wakeup_src_Queue_Send(buf);
 		break;
 	default:
